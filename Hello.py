eaa6eb4bd13fe5a92fc769b6ea8187d827f1a402
Prabhakar-03 <104347081+Prabhakar-03@users.noreply.github.com>
2024-02-13 17:40:02 +0530

    Initial commit

---
+++ Hello.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Diet and Workout Recommendation System! 👋")
+
+
+st.markdown(
+    """
+    A diet recommendation web application using content-based approach with Scikit-Learn, FastAPI and Streamlit.
+    Our Diet recommendation engine, using a content-based approach is an important tool for promoting healthy eating habits.
+    This type of engine uses information about the nutritional content and ingredients of foods to make personalized recommendations 
+    to users. 
+
+    """
+)
